48de504867960f5e3ac36681fa2a7bb007696892
reddy <reddyfede@gmail.com>
2023-08-12 18:35:07 +0200

    Add local storage logic for order/index

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,27 +1,22 @@
 const Order = require('../models/order')
-const localStorage = require('node-localstorage')
 
 module.exports = {
     index,
 }
 
 async function index(req, res) {
-    //check localstorage
-    //if order exists, set active order to that id
-    //otherwise
-    //create a new order
-    //set localstorage
+    let currOrder = {}
 
     if (typeof localStorage === "undefined" || localStorage === null) {
-        var LocalStorage = require('node-localstorage').LocalStorage;
+        const LocalStorage = require('node-localstorage').LocalStorage;
         localStorage = new LocalStorage('./scratch');
     }
-    localStorage.setItem("orderID", "123456789");
-    if (localStorage.getItem("orderID") === null) {
-        console.log("NULL CASE, ID:", localStorage.getItem("orderID"))
-    } else {
-        console.log("NOT NULL CASE, ID;", localStorage.getItem("orderID"))
-    }
 
+    if (localStorage.getItem("orderID") === null) {
+        currOrder = await Order.create({}) 
+        localStorage.setItem("orderID",`${currOrder._id}`)
+    } else {
+        currOrder = await Order.findById(localStorage.getItem("orderID"))
+    }
     res.render('order/index', { title: "Order", order: currOrder })
 }
