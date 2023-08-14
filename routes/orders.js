9b51063fb73f00661d2aad6a31d78094869d6143
SpencerlElswick <spencerlelswick@gmail.com>
2023-08-14 02:25:24 -0400

    Add test checkout page

--- routes/orders.js
+++ routes/orders.js
@@ -5,11 +5,13 @@ const orderCtrl = require('../controllers/orders')
 router.get('/builder', orderCtrl.newBuild);
 router.post('/builder', orderCtrl.createBuild);
 router.get('/cart', orderCtrl.show)
+router.get('/checkout/:id', orderCtrl.checkout)
 router.get('/', orderCtrl.index);
 router.delete('/cart/:id', orderCtrl.deleteItem);
 router.post('/cart/:id', orderCtrl.editQuantity);
 
 router.post('/builder/edit/:id', orderCtrl.editBuild);
 router.post('/builder/:id', orderCtrl.saveBuild);
+
 
 module.exports = router;
