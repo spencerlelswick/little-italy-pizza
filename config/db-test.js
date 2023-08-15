5edf7ed9f9ca72653b2264825483a771cd2c1abf
reddy <reddyfede@gmail.com>
2023-08-15 07:43:17 +0200

    add favicon

--- config/db-test.js
+++ config/db-test.js
@@ -1,2 +1,37 @@
-require('dotenv').config()
-require('./database')
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL);
+
+const db = mongoose.connection;
+	
+db.on('connected', function() {
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
+
+const pizzaSchema = new Schema(
+    {
+      items: {
+        pizzas:[],
+        sides: []
+      },
+      total: {
+        type: Number,
+        default: 0,
+        min: 0,
+      },
+      status: {
+        type: String,
+        default: 'received',
+        enum: ["received", "confirmed", "prepare", "bake", "deliver", "complete"]
+      },
+    },
+    {
+        
+
+    
+
+    },
+    {
+      timestamps: true,
+    }
+  );
