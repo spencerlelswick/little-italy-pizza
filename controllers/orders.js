7da371791da132b0f699070ee52cf514e781e323
reddy <reddyfede@gmail.com>
2023-08-15 17:15:29 +0200

    refactor editQuantity function

--- controllers/orders.js
+++ controllers/orders.js
@@ -102,70 +102,29 @@ async function show(req, res) {
     res.render('cart/index', { title: "Cart", order: order })
 }
 
+//perfect
 async function deleteItem(req, res) {
     const orderId = req.cookies.orderId
     const itemId = req.params.id
-
-    const order = await Order.findById(orderId)
-
-
-    newItems = {...order.items}
-    let index = -1
-    
-    if (newItems.pizzas.length){
-        index = newItems.pizzas.findIndex(pizza => pizza.id === itemId)
-        if (index !== -1) {
-            newItems.pizzas.splice(index, 1)
-        }
-    }
-    
-    if(newItems.sides.length && index === -1){
-        index = newItems.sides.findIndex(side => side.id === itemId)
-        newItems.sides.splice(index, 1)
-    }
-
-    let newTotal = Helper.totalOrder(newItems)
-    await Order.findOneAndUpdate(
-        { _id: orderId },
-        { $set: { items: newItems, total: newTotal } }
-    );
+    await Pizza.findByIdAndDelete(itemId)
+    const order = await Order.findById(orderId).populate('items.pizzas')
+    order.total = calcTotal(order.items)
+    order.save()
     res.redirect('/order/cart')
 }
 
 async function editQuantity(req, res) {
     const orderId = req.cookies.orderId
     const itemId = req.params.id
-    let newQty = parseInt(req.body.qty)
-    const order = await Order.findById(orderId)
-    newItems = {...order.items}
-    let index = -1
-    if (newItems.pizzas.length){
-        index = newItems.pizzas.findIndex(pizza => pizza.id === itemId)
-        if (index !== -1) {
-            let oldQty = newItems.pizzas[index].quantity
-            if (parseInt(oldQty) <= 0) {
-                newItems.pizzas[index].quantity = 1+""
-            } else if (!(parseInt(oldQty) <= 1 && newQty === -1)) {
-                newItems.pizzas[index].quantity = parseInt(oldQty) + newQty+""
-            }
-        }
+    const newQty = parseInt(req.body.qty)
+    const pizza = await Pizza.findById(itemId)
+    if (!(pizza.quantity === 1 && newQty === -1)){
+        pizza.quantity += newQty
+        pizza.save()
     }
-    
-    if(newItems.sides.length &&  index === -1){
-        index = newItems.sides.findIndex(side => side.id === itemId)
-        let oldQty = newItems.sides[index].quantity
-        if (parseInt(oldQty) <= 0) {
-            newItems.sides[index].quantity = 1+""
-        } else if (!(parseInt(oldQty) <= 1 && newQty === -1)) {
-            newItems.sides[index].quantity = parseInt(oldQty) + newQty+""
-        }
-    }
-
-    let newTotal = Helper.totalOrder(newItems)
-    await Order.findOneAndUpdate(
-        { _id: orderId },
-        { $set: { items: newItems, total: newTotal } }
-    );
+    const order = await Order.findById(orderId).populate('items.pizzas')
+    order.total = calcTotal(order.items)
+    order.save()
     res.redirect('/order/cart')
 }
 
