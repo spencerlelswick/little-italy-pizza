f5d69d3ff7b0d91dd1f159c1606aeebe743c78ff
reddy <reddyfede@gmail.com>
2023-08-16 14:35:42 +0200

    fix interaction with cart and error page

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {title: "Error"});
+  res.render('error', {title: "Error", order:{items:{ pizzas:[],sides:[]}}});
 });
 
 module.exports = app;
